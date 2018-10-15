39e946e75d2b79915166f3f5e64fd446c548e96e
Nathan Graule <solarliner@gmail.com>
2018-10-15 12:19:53 +0200

    Refer to "index.html" if target is a directory on static handler

--- src/router/handlers.ts
+++ src/router/handlers.ts
@@ -1,14 +1,18 @@
 import { ResponseHandler } from ".";
 import { map } from "rxjs/operators";
 import { join } from "path";
-import { createReadStream } from "fs";
+import { createReadStream, lstatSync } from "fs";
 import { lookup } from "mime-types";
+import { lstat } from "fs-extra";
 
 export function staticHandler(directory: string, base: string): ResponseHandler {
   if (!base.endsWith("/")) base += "/";
   return map(({ path }) => {
-    const relPath = path.replace(base, "").replace(/\/$/, "");
-    const filepath = join(directory, relPath);
+    const relPath = path.replace(base, "").replace(/^\/|\/$/, "");
+    let filepath = join(directory, relPath);
+    if (lstatSync(filepath).isDirectory()) {
+      filepath = join(filepath, "index.html");
+    }
     const fstream = createReadStream(filepath);
     const mime = lookup(filepath);
     if (!mime) {
