56db2a75685b79bca28a861b487557bf8554b0d4
Nathan Graule <solarliner@gmail.com>
2018-11-25 01:44:54 +0100

    Implement changes from base server into http server

--- packages/core/src/http.ts
+++ packages/core/src/http.ts
@@ -3,16 +3,20 @@ import { Observable } from "rxjs";
 import { IServerRequest } from ".";
 import { BaseServer } from "./base";
 export class HTTPServer extends BaseServer {
-  protected _obs: Observable<IServerRequest>;
+  private obs: Observable<IServerRequest>;
 
   constructor(port: number) {
     super();
-    this._obs = new Observable<IServerRequest>(sub => {
+    this.obs = new Observable<IServerRequest>(sub => {
       const server = new Server((req, res) => {
         sub.next({ req, res, extra: {} });
       });
       server.on("close", () => sub.complete());
       server.listen(port);
     });
+  }
+
+  public get observer() {
+    return this.obs;
   }
 }
