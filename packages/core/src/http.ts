2f226cdbbd1b00a8cfe676094f68e815323b1336
Nathan Graule <solarliner@gmail.com>
2018-11-25 01:47:25 +0100

    Implement HTTPS server

--- packages/core/src/http.ts
+++ packages/core/src/http.ts
@@ -2,6 +2,7 @@ import { Server } from "http";
 import { Observable } from "rxjs";
 import { IServerRequest } from ".";
 import { BaseServer } from "./base";
+import { ServerOptions, createServer } from "https";
 export class HTTPServer extends BaseServer {
   private obs: Observable<IServerRequest>;
 
@@ -11,6 +12,25 @@ export class HTTPServer extends BaseServer {
       const server = new Server((req, res) => {
         sub.next({ req, res, extra: {} });
       });
+      server.on("close", () => sub.complete());
+      server.listen(port);
+    });
+  }
+
+  public get observer() {
+    return this.obs;
+  }
+}
+
+export class HTTPSServer extends BaseServer {
+  private obs: Observable<IServerRequest>;
+
+  constructor(port: number, options: ServerOptions) {
+    super();
+    this.obs = new Observable<IServerRequest>(sub => {
+      const server = createServer(options || {}, (req, res) => {
+        sub.next({ req, res, extra: {} });
+      })
       server.on("close", () => sub.complete());
       server.listen(port);
     });
