3ff55ecbadc6d3e5bb5313cf93f40add216eca69
Nathan Graule <solarliner@gmail.com>
2019-03-02 20:49:39 +0100

    Fix mount middleware

--- packages/bayerjs-mount/src/index.ts
+++ packages/bayerjs-mount/src/index.ts
@@ -1,32 +1,25 @@
-import assert from "assert";
-
-import _debug from "debug";
-import { of, pipe } from "rxjs";
-import { filter, map, mergeMap } from "rxjs/operators";
-
 import { ServerMiddleware } from "@bayerjs/core";
+import assert from "assert";
+import _debug from "debug";
+import { of } from "rxjs";
+import { mergeMap } from "rxjs/operators";
 
 const debug = _debug("@bayerjs/mount");
 
 export default function mount(mountPath: string, middleware: ServerMiddleware): ServerMiddleware {
   assert.equal(mountPath[0], "/", "Mount path must be absolute");
-  const trailingSlash = mountPath.slice(-1) === "/";
   debug("Mounting middleware to %o", mountPath);
-  return pipe(
-    filter(({ req }) => {
-      debug("Request test for %o on %o", req.route.path, mountPath);
-      return match(mountPath, req.route.path, trailingSlash);
-    }),
-    map(({ req: oldReq, res, extra }) => {
-      const req = oldReq.clone(mountPath);
-      debug("Mapping request %o %o", oldReq.route.path, req.route.path);
-      return { req, res, extra };
-    }),
-    mergeMap(ctx => {
-      debug("Passing to mounted middleware from %o %s", mountPath, ctx.req.route.path);
-      return of(ctx).pipe(middleware);
-    })
-  );
+  return mergeMap(ctx => {
+    const requestPath = ctx.req.route.path;
+    if (!match(mountPath, requestPath)) {
+      debug("Unrelated request %s, passing along...", requestPath);
+      return of(ctx);
+    }
+    debug("Passing request %s to mounted middleware", requestPath);
+    const req = ctx.req.clone(mountPath);
+    const newCtx = Object.assign({}, ctx, { req });
+    return of(newCtx).pipe(middleware);
+  })
 }
 
 function match(prefix: string, path: string, trailingSlash = false) {
